e06bf93f91500a12fdc82f65f7bc003298c7c9a3
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-17 14:06:00 -0600

    when functions for answers

--- index.js
+++ index.js
@@ -1,22 +1,89 @@
 
 var jest = require('jest');
 var inquirer = require('inquirer');
-const Employee = require('../lib/Employee.js');
-const Engineer = require('../lib/Engineer.js');
-const Intern = require('../lib/Intern.js');
-const Manager = require('../lib/Manager.js');
+const fs = require('fs')
+// const Employee = require('../lib/Employee.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Manager.js');
 // const generatePage = require('./src/page-template');
 
 
 //  CLASSES AND PROTOTYPES
 // https://gist.github.com/prof3ssorSt3v3/c056b8b5f379ee2767bb4e8ad90f3dac
 
-
+// common questions
 const questions = [
 
-        
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is team member's name?",
 
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is team member's id?",
+
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is team member's email?",
+
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: "What is team member's role?",
+        choices: ['Engineer', 'Intern', 'Manager' ]
+    },
+    {  
+        when: function( answers ) {
+           if(answers.role === 'Engineer')
+           return true;
+          },
+
+    type: 'input',
+    message: "Engineer: What is team member's Github username?",
+    name: 'github',
+    
+    },
+    {  
+        when: function( answers ) {
+           if(answers.role === 'Intern')
+           return true;
+          },
+
+    type: 'input',
+    message: "Intern: Where did team member go to school?",
+    name: 'school',
+    
+    },
+    {  
+        when: function( answers ) {
+           if(answers.role === 'Manager')
+           return true;
+          },
+
+    type: 'input',
+    message: "Manager: What is team member's office number?",
+    name: 'officeNumber',
+    
+    },
 
 
 
 ];
+
+/// when: answers 
+// https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/when.js#L18
+
+
+
+const team = [];
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+  });
+
